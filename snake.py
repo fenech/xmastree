d0257953e462f44d700aad47f35eea67cf965711
Tom Fenech <tomjwfenech@gmail.com>
2019-12-31 19:39:31 +0100

    Add more effects and refactor

--- snake.py
+++ snake.py
@@ -2,45 +2,42 @@ from gpiozero import PiHutXmasTree
 from gpiozero.tools import random_values
 from time import sleep
 
-tree = PiHutXmasTree(pwm=True)
-
-indices = [19, 22, 4, 23, 8, 2, 16, 15, 17, 13, 24, 20, 6, 7, 12, 3, 14, 1, 10, 11, 21, 9, 5, 18]
-lights = [tree[i] for i in indices]
-num_lights = len(indices)
-
-def reset():
+def reset(lights):
   for light in lights:
     light.off()
 
-def snake(i):
+
+def snake_step(i):
   lights[i%num_lights].on()
   lights[(i+1)%num_lights].on()
   lights[(i+2)%num_lights].on()
   lights[(i-1)%num_lights].off()
 
-reset()
 
-tree.star.source_delay = 0.1
-tree.star.source = random_values()
-
-while True:
+def snake(lights):
   for _ in range(3):
-    for i, _ in enumerate(indices):
-      snake(i)
-      snake(i+num_lights//2)
+    for i, _ in enumerate(lights):
+      snake_step(i)
+      snake_step(i+num_lights//2)
       sleep(0.1)
 
-  reset()
+  reset(lights)
 
-  clone = lights.copy()
-  while len(clone) > 0:
-    for i, _ in enumerate(clone):
-      lights[i].on()
-      if i > 0:
-        lights[i-1].off()
-      sleep(0.05)
-    clone.pop()
 
+def chase(lights):
+  if len(lights) == 0:
+    return
+
+  for i, _ in enumerate(lights):
+    lights[i].on()
+    if i > 0:
+      lights[i-1].off()
+    sleep(0.05)
+
+  chase(lights[:-1])
+
+
+def flicker(lights):
   for light in lights:
     light.source_delay = 0.1
     light.source = random_values()
@@ -49,3 +46,44 @@ while True:
 
   for light in lights:
     light.source = None
+
+
+def blink(lights):
+  for _ in range(8):
+    thirds = len(lights) // 3
+    for i in range(3):
+      for light in tree[i*thirds:(i+1)*thirds]:
+        light.on()
+      sleep(0.2)
+      for light in tree[i*thirds:(i+1)*thirds]:
+        light.off()
+
+
+def dnb(tree):
+  for r in range(1, 6):
+    for _ in range(2, 2**r):
+      for light in tree:
+        light.on()
+      sleep(1/(2**r))
+      for light in tree:
+        light.off()
+      sleep(1/(2**r))
+
+
+tree = PiHutXmasTree(pwm=True)
+
+indices = [19, 22, 4, 23, 8, 2, 16, 15, 17, 13, 24, 20, 6, 7, 12, 3, 14, 1, 10, 11, 21, 9, 5, 18]
+lights = [tree[i] for i in indices]
+num_lights = len(indices)
+
+reset(lights)
+
+tree.star.source_delay = 0.1
+tree.star.source = random_values()
+
+while True:
+  snake(lights)
+  chase(lights)
+  flicker(lights)
+  blink(lights)
+  dnb(tree)
